f036bd0f1704fc559c6562f2bb1036b4daa9a0cd
Khas-Erdene-Tsogtsaikhan <khaserdene_ts@berkeley.edu>
2026-07-12 00:16:56 +0900

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.py
@@ -0,0 +1,54 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        #hashmap everythign and then iterate thorugh thte lsit
+        #if nums[0]+1 exists 
+
+
+        # have a )(N ) look up and then interate through every number keep looking up if num[i]-1 exists
+        # then with teh length intialied we can then count from then on
+
+        seen = {}
+        res = 0
+
+        for i in range(len(nums)):
+            seen[nums[i]] = seen.get(nums[i], 1)
+        
+        for i in range(len(nums)):
+            length = 1
+            curr = nums[i]
+            while curr-1 in seen:
+                length += 1
+                curr -=1
+            curr = nums[i]
+
+            while curr + 1 in seen:
+                length += 1
+                curr +=1
+            res = max(res, length)
+        return res
+
+
+
+           
+
+
+            # nums = [3, 2,20,4,10,3,4,5]
+
+        
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
